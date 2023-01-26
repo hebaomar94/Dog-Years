e4c07f3b04543d9ed0719bd4017091a703a85e64
Heba Omar <97067717+hebaomar94@users.noreply.github.com>
2023-01-26 17:57:34 +0200

    Create index.js

---
+++ index.js
@@ -0,0 +1,22 @@
+//creat variables age
+let myAge = 25;
+//Create a variable earlyyears
+let earlyYears = 2;
+
+earlyYears *= 10.5;
+
+// subtract 2 from myage the result equal lateryears
+let laterYears = myAge - 2;
+
+//Multiply the laterYears variable by 4 to calculate the number of dog years accounted for by your later years.
+laterYears *= 4;
+console.log(laterYears);
+
+//store earlyyears and lateryears in variable
+let myAgeIndDogYears = earlyYears + laterYears;
+//write my name with lowercase
+let myName = 'Heba' .toLowerCase();
+//print it in console
+console.log(`My name is ${myName}.
+ I am ${myAge} years old in human years which is ${myAgeIndDogYears} years old in dog years.`)
+
